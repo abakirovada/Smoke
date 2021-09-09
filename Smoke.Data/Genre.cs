2492690352e5c629e3897d55ac5b8854634d1aa0
Dan Kallman <dkallman13@gmail.com>
2021-09-09 12:03:20 -0400

    added genre data class

---
+++ Smoke.Data/Genre.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smoke.Data
+{
+    public class Genre
+    {
+        [Key]
+        public int GenreId { get; set; }
+
+        [Required]
+        public string GenreName { get; set; }
+
+
+    }
+    //public enum Genres {Shooter, Fighting, Stealth, Survival, Rhythm, BattleRoyale, Horror, Adventure, ActionRPG, MMORPG, Roguelike, RogueLite, JRPG, Simulation, FourX, MOBA, RealTimeStrategy, TowerDefense, TurnBasedStrategy, GrandStrategy, Sports, Racing, MMO, Idle, Board, Educatonal, Sandbox, OpenWorld, SoulsLike}
+}
