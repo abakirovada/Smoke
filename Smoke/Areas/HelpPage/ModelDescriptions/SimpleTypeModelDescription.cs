d57f1e1943996446d8eb743d5c796360ea46a5bf
Dinara Abakirova <abakirovada@gmail.com>
2021-09-09 09:39:56 -0400

    Initial commit

---
+++ Smoke/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Smoke.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Smoke/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Smoke.WebApiApplication" Language="C#" %>
