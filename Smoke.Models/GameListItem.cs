a06244707010c1fb53437f9539acb7094760ce56
Dinara Abakirova <abakirovada@gmail.com>
2021-09-09 12:02:44 -0400

    Game model, service, data added

---
+++ Smoke.Models/GameListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smoke.Models
+{
+    public class GameListItem
+    {
+        public Guid GameId { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ Smoke.Services/GameService.cs
@@ -0,0 +1,78 @@
+﻿using Smoke.Data;
+using Smoke.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smoke.Services
+{
+    public class GameService
+    {
+        private readonly Guid _userId;
+        public GameService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateGaame(GameCreate model)
+        {
+            var entity = new Game()
+            {
+                GameId = _userId,
+                Name = model.Name,
+                GameStore=model.GameStore,
+                Platform = model.Platform,
+                Genre = model.Genre,
+                MaturityRating = model.MaturityRating,
+                Modding = model.Modding
+            };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Games.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<GameListItem> GetGames()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Games
+                    .Where(e => e.GameId == _userId)
+                    .Select(
+                        e =>
+                        new GameListItem
+                        {
+                            GameId = e.GameId,
+                            Name = e.Name
+                        }
+                        );
+                return query.ToArray();
+            }
+        }
+        public GameDetail  GetGameById(Guid id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx.Games
+                    .Single(e => e.GameId == id && e.GameId == _userId);
+                return new GameDetail
+                {
+                    GameId = _userId,
+                    Name = entity.Name,
+                    GameStore = entity.GameStore,
+                    Platform = entity.Platform,
+                    Genre = entity.Genre,
+                    MaturityRating = entity.MaturityRating,
+                    Modding = entity.Modding
+                };
+            }
+        }
+    }
+}
