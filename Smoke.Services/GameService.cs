eb1cc2676143077cdc936733af18873b533fcd82
Duncan Summers <djsummers36@gmail.com>
2021-09-09 12:02:36 -0400

    mod support

---
+++ Smoke.Services/GameService.cs
@@ -0,0 +1,22 @@
+﻿using Smoke.Data;
+using Smoke.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smoke.Services
+{
+    public class GameService
+    {
+        public bool CreateGame(GameCreate model)
+        {
+            var entity =
+                new Game()
+                {
+
+                }
+        }
+    }
+}
